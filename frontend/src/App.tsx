3da2bcf73bc3298648d528266fcd90403571b47e
eduardo-victor <victorofish@gmail.com>
2023-12-12 10:17:13 -0300

    feat: fetching api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
 import axios from "axios"
 
 function App() {
 
+  const [lanches, setLanches] = useState([]);
+
   const fetch = () => {
-    axios.get('http://localhost:7000/')
+    axios.get('http://localhost:7000/api/menu/items')
     .then((res) => {
-      console.log(res.data)
+      setLanches(res.data)
     })
   }
 
   return (
     <>
       <div>
+        <div>
+          {lanches?.map(item => (
+            <div>
+              <h1>{item.name}</h1>
+            </div>
+          ))}
+        </div>
         <button onClick={fetch}>TESTA</button>
+        
       </div>
     </>
   )
